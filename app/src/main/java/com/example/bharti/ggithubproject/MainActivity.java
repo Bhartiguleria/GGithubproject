e0bce145642d0192a9e626506d853ac2fa511a4a
Bhartiguleria <bguleria1997@gmail.com>
2019-03-07 12:27:17 +0530

    edittext value on toast

--- app/src/main/java/com/example/bharti/ggithubproject/MainActivity.java
+++ app/src/main/java/com/example/bharti/ggithubproject/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.bharti.ggithubproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button b;
+    EditText e1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b=(Button)findViewById(R.id.button);
+        e1=(EditText) findViewById(R.id.editText);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String h=e1.getText().toString();
+                Toast.makeText(MainActivity.this, h, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
